cb18b655dcfdf9c13cc9b607fbb5600b1aeb2300
Sathya2020 <62892486+Sathya2020@users.noreply.github.com>
2020-03-31 07:06:19 +0530

    Add files via upload

---
+++ mar30.py
@@ -0,0 +1,199 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+8.0
+>>> 
+==================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+8.0
+45
+>>> 
+==================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+45
+>>> 
+==================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+<class 'str'>
+45
+>>> 
+==================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+<class 'str'>
+45
+Traceback (most recent call last):
+  File "D:\Python\Besent Tech\mar30.py", line 15, in <module>
+    g = boolean(d)
+NameError: name 'boolean' is not defined
+>>> 
+==================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+<class 'str'>
+45
+True
+<class 'bool'>
+>>> 
+==================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+<class 'str'>
+45
+True
+<class 'bool'>
+Traceback (most recent call last):
+  File "D:\Python\Besent Tech\mar30.py", line 20, in <module>
+    w = int(q)
+ValueError: invalid literal for int() with base 10: 'good'
+>>> 
+==================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+<class 'str'>
+45
+True
+<class 'bool'>
+Traceback (most recent call last):
+  File "D:\Python\Besent Tech\mar30.py", line 20, in <module>
+    w = int(q)
+ValueError: invalid literal for int() with base 10: 'good'
+>>> 
+==================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+<class 'str'>
+45
+True
+<class 'bool'>
+Traceback (most recent call last):
+  File "D:\Python\Besent Tech\mar30.py", line 20, in <module>
+    w = int(q)
+ValueError: invalid literal for int() with base 10: 'good'
+>>> 
+==================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+<class 'str'>
+45
+True
+<class 'bool'>
+Traceback (most recent call last):
+  File "D:\Python\Besent Tech\mar30.py", line 20, in <module>
+    w = int(q)
+ValueError: invalid literal for int() with base 10: 'good'
+>>> 
+==================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+<class 'str'>
+45
+True
+<class 'bool'>
+Traceback (most recent call last):
+  File "D:\Python\Besent Tech\mar30.py", line 20, in <module>
+    w = int(q)
+ValueError: invalid literal for int() with base 10: 'good'
+>>> 
+==================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+<class 'str'>
+45
+True
+<class 'bool'>
+Traceback (most recent call last):
+  File "D:\Python\Besent Tech\mar30.py", line 20, in <module>
+    w = int(q)
+ValueError: invalid literal for int() with base 10: 'good'
+>>> 
+==================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+<class 'str'>
+45
+True
+<class 'bool'>
+Traceback (most recent call last):
+  File "D:\Python\Besent Tech\mar30.py", line 20, in <module>
+    w = int(q)
+ValueError: invalid literal for int() with base 10: 'good'
+>>> 
+==================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+<class 'str'>
+45
+True
+<class 'bool'>
+Traceback (most recent call last):
+  File "D:\Python\Besent Tech\mar30.py", line 20, in <module>
+    w = int(q)
+ValueError: invalid literal for int() with base 10: 'good'
+>>> 
+==================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+<class 'str'>
+45
+True
+<class 'bool'>
+Traceback (most recent call last):
+  File "D:\Python\Besent Tech\mar30.py", line 20, in <module>
+    w = int(q)
+ValueError: invalid literal for int() with base 10: 'good'
+>>> 
+=================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+<class 'str'>
+45
+True
+<class 'bool'>
+Traceback (most recent call last):
+  File "D:\Python\Besent Tech\mar30.py", line 20, in <module>
+    w = int(q)
+ValueError: invalid literal for int() with base 10: 'good'
+>>> 
+=================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+<class 'str'>
+45
+True
+<class 'bool'>
+Traceback (most recent call last):
+  File "D:\Python\Besent Tech\mar30.py", line 20, in <module>
+    w = int(q)
+ValueError: invalid literal for int() with base 10: 'good'
+>>> 
+=================== RESTART: D:\Python\Besent Tech\mar30.py ===================
+<class 'int'>
+8.0
+<class 'float'>
+<class 'str'>
+45
+True
+<class 'bool'>
+Traceback (most recent call last):
+  File "D:\Python\Besent Tech\mar30.py", line 20, in <module>
+    w = int(q)
+ValueError: invalid literal for int() with base 10: 'good'
+>>> 
